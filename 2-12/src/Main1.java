e6fad0f9cf964363fe546ece58a55cf4849a6707
yumi-okada <i.rumera.192@gmail.com>
2026-04-15 10:06:17 +0900

    Add files via upload

---
+++ 2-12/src/Main1.java
@@ -0,0 +1,7 @@
+public class Main1 {
+    public static void main(String[] args) {
+        for (int count = 10; count > 0; count--){
+            System.out.println(count);
+        }
+    }
+}
---
+++ 2-12/src/Main2.java
@@ -0,0 +1,10 @@
+public class Main2 {
+    public static void main(String[] args) {
+        for (int dan = 1; dan <= 5; dan++){
+            for (int num = 1; num <= 9; num++){
+                System.out.println(dan + " × " + num + " = " + (dan * num));
+            }
+            System.out.println();
+        }
+    }
+}
