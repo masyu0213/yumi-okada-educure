e9a0888a73bada05e4a14d4a36e9ee3eb1f2ab13
yumi-okada <okada@kanaeru.co.jp>
2026-05-21 12:50:32 +0900

    修正

--- 2-22/src/Main.java
+++ 2-22/src/Main.java
@@ -1,14 +1,13 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String[] abc = new String[20];
+        String[] englishWords = new String[20];
         String[] japaneseWords = new String[20];
         int wordCount = 0;
         int score;
         Scanner scanner = new Scanner(System.in);
-        int choice;
+        String choice;
 
         do {
             System.out.println("メニューを選択してください");
@@ -16,55 +15,48 @@ public class Main {
             System.out.println("2: クイズを実施");
             System.out.println("3: 終了");
 
-            choice = scanner.nextInt();
+            choice = scanner.next();
 
             switch (choice) {
-                case 1:
+                case "1":
                     if (wordCount < 20) {
-                        System.out.println("英単語を入力してください");
-                        abc[wordCount] = scanner.next();
-                        System.out.println("日本語訳を入力してください");
+                        System.out.println("英単語を入力してください:");
+                        englishWords[wordCount] = scanner.next();
+                        System.out.println("日本語訳を入力してください:");
                         japaneseWords[wordCount] = scanner.next();
                         wordCount++;
                     } else {
                         System.out.println("登録可能な単語数は20個までです");
                     }
                     break;
-                case 2:
+                case "2":
                     if (wordCount == 0) {
                         System.out.println("単語が登録されていません");
                         break;
                     }
-                    int remainingWords = wordCount;
                     score = 0;
-                    Random rand = new Random();
+
                     for (int i = 0; i < wordCount; i++) {
-
-                        int index = rand.nextInt(remainingWords);
-
-                        System.out.println(abc[index]);
+                        System.out.println(englishWords[i] + "の意味は？");
                         String answer = scanner.next();
-                        if (answer.equals(japaneseWords[index])) {
-                            System.out.println("正解です");
+                        if (answer.equals(japaneseWords[i])) {
+                            System.out.println("正解です！");
                             score++;
                         } else {
                             System.out.println("不正解です");
                         }
-
-                        abc[index] = abc[remainingWords - 1];
-                        japaneseWords[index] = japaneseWords[remainingWords - 1];
-                        remainingWords--;
                     }
+                    System.out.println("クイズ終了！");
                     System.out.println(wordCount + "問中 " + score + "問正解でした！");
                     break;
-                case 3:
+                case "3":
                     System.out.println("終了します");
                     break;
                 default:
                     System.out.println("1-3の数字を入力してください");
                     break;
             }
-        } while (choice != 3);
+        } while (!choice.equals("3"));
         scanner.close();
     }
 }
