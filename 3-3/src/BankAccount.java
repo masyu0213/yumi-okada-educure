3118f81bdc9b6b8392c4da35e91d7c027c6c162a
yumi-okada <okada@kanaeru.co.jp>
2026-05-23 16:39:21 +0900

    メソッド修正

--- 3-3/src/BankAccount.java
+++ 3-3/src/BankAccount.java
@@ -1,21 +1,19 @@
 public class BankAccount {
     int balance;
 
-    public void getBalance() {
-        System.out.println("残高: " + balance + "円");
+    public int getBalance() {
+        return balance;
     }
 
     public void deposit(int amount) {
         balance += amount;
         System.out.println(amount + "円預けました");
-        System.out.println("残高: " + balance + "円");
     }
 
     public void withdraw(int amount) {
         if (balance >= amount) {
             balance -= amount;
             System.out.println(amount + "円引き出しました");
-            System.out.println("残高: " + balance + "円");
         } else {
             System.out.println("残高が不足しています");
         }
--- 3-3/src/Main3.java
+++ 3-3/src/Main3.java
@@ -1,10 +1,12 @@
 public class Main3 {
     public static void main(String[] args) {
         BankAccount myAccount = new BankAccount();
-        myAccount.getBalance();
-        
+
+        System.out.println("残高: " + myAccount.getBalance() + "円");
         myAccount.deposit(10000);
+        System.out.println("残高: " + myAccount.getBalance() + "円");
         myAccount.withdraw(3000);
+        System.out.println("残高: " + myAccount.getBalance() + "円");
         myAccount.withdraw(8000);
     }
 }
