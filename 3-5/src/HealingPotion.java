2d47900377c0c8de4bb448634d17b5f99a417601
masyu0213 <konr01271@gmail.com>
2026-05-26 14:48:42 +0900

    Main3,Main1,HealingPotion変更

--- 3-5/src/HealingPotion.java
+++ 3-5/src/HealingPotion.java
@@ -2,7 +2,6 @@ public class HealingPotion {
     public void heal(Player player) {
         player.hp += 50;
         System.out.println("回復ポーションを使用しました");
-        System.out.println("プレイヤーのHP: " + player.hp);
     }
     public void displayInfo(Player player) {
         System.out.println("プレイヤー名: " + player.name);
